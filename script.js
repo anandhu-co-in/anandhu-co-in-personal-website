565bb6b4e0b1a4dfaf75931ebceb710d656b71fb
anandhu-co-in <mail.anandhu@gmail.com>
2021-11-24 15:28:32 +0530

    .

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ var hidetimer=window.setInterval(fun,1000)
 
 f1.onmouseover = function(e) {
     clearInterval(hidetimer);
-    msg.innerHTML = "I have 3.7 Years of Industry Experience in Manual & Automation Testing of Web & Mobile Applications.<br/>Have worked on Selenium, Java, TestNG, Appium, Jira, qTest, Perfecto and Cucumber";
+    msg.innerHTML = "I have 3.7 Years of Industry Experience in Manual & Automation Testing of Web & Mobile Applications.<br/>Have worked on Selenium, Java, TestNG, Appium, Jira, qTest, Perfecto and Cucumber.";
     msg.classList.add("fullOpacity")
 };
 
 f2.onmouseover = function(e) {
     clearInterval(hidetimer);
     msg.classList.add("fullOpacity")  
-    msg.innerHTML = "Certified RPA Developer.<br/>Experienced in developing Process Automation Solutions using UiPath, BluePrism and Selenium";
+    msg.innerHTML = "Certified RPA Developer.<br/>Experienced in developing Process Automation Solutions using UiPath, BluePrism and Selenium.";
 };
 
 f3.onmouseover = function(e) {
    clearInterval(hidetimer);
    msg.classList.add("fullOpacity")
-   msg.innerHTML = "Self learned MERN Stack Developer through Debug Media & Brototype.<br/>Have good knowledge in HTML, CSS, JavaScript, NodeJS, ReactJS, ExpressJS, Redux, PHP & UI/UX Designing";
+   msg.innerHTML = "Self learned MERN Stack Developer through Debug Media & Brototype.<br/>Have good knowledge in HTML, CSS, JavaScript, NodeJS, ReactJS, ExpressJS, Redux, PHP & UI/UX Designing.";
 };
 
 f1.onmouseout = f2.onmouseout = f3.onmouseout = function(e) {
