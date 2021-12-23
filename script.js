a753b4d8d80ce6686cb480d176f08a1f3abe7c74
codeandcoins <mail.anandhu@gmail.com>
2021-12-23 22:42:21 +0530

    fixes

--- script.js
+++ script.js
@@ -182,3 +182,12 @@ function hideTypingDots(){
     location.reload(true);
  }
 
+
+
+
+ var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
